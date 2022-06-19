2a73e6c54f42a19491e6e1f6b1e309d8d39b7a01
Oleg <kombo007@mail.ru>
2022-06-19 14:16:25 +0700

    Updates

---
+++ precache-manifest.fbe254171c6b57d141fb0b2804189ad8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e12ba24eca5aa9d0ca77",
+    "url": "/tasks-manager/static/css/main.d101bae2.chunk.css"
+  },
+  {
+    "revision": "e12ba24eca5aa9d0ca77",
+    "url": "/tasks-manager/static/js/main.e12ba24e.chunk.js"
+  },
+  {
+    "revision": "9d4fcf12a6d34e773b75",
+    "url": "/tasks-manager/static/css/1.da2b7212.chunk.css"
+  },
+  {
+    "revision": "9d4fcf12a6d34e773b75",
+    "url": "/tasks-manager/static/js/1.9d4fcf12.chunk.js"
+  },
+  {
+    "revision": "ac819cd69b29e2762820",
+    "url": "/tasks-manager/static/js/runtime~main.ac819cd6.js"
+  },
+  {
+    "revision": "ac3cf13104e0c28ddead1778ccbcf09d",
+    "url": "/tasks-manager/static/media/back.ac3cf131.png"
+  },
+  {
+    "revision": "a3fcdff002f26cdf718968593b0d9e47",
+    "url": "/tasks-manager/static/media/Montserrat-VariableFont_wght.a3fcdff0.ttf"
+  },
+  {
+    "revision": "9ed87bd82896b852fcbea9a0b90eed1d",
+    "url": "/tasks-manager/index.html"
+  }
+];
